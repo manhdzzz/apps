b13ec9c9f45471043a0a5582a64c31f0131589d4
menjmoi <101183159+manhdzzz@users.noreply.github.com>
2024-10-31 19:09:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 async function fetchAppsData() {
     try {
-        const response = await fetch('https://raw.githubusercontent.com/manhdzzz/apps/refs/heads/main/apps.js');
+        const response = await fetch('https://raw.githubusercontent.com/manhdzzz/apps/refs/heads/main/apps');
         const data = await response.json();
 
         const apps = data.apps;
